2d2c1bf1ccbdc6f55bf52c653f97470df7fd8edb
Joshua A Jack <mynameisjoshuajack@gmail.com>
2020-03-05 01:31:05 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,9 +1,9 @@
 class Person 
   def talk 
-    print "Hello World!"
+    puts "Hello World!"
   end 
   def walk 
-    print "The Person is walking"
+    puts "The Person is walking"
   end 
   
   ada = Person.new 
